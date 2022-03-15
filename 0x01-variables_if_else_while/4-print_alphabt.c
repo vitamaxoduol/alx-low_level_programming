69cb8fe1bcaf5ac76756d0b7551453d3b7861356
vitamax <vitamaxoduo@gmail.com>
2022-03-15 00:40:44 -0700

    alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,27 @@
-#include <stdio.h>
-
-/**
- * main - prints the alphabet in lowercase,
- * followed by a new line, except q and e
- * Return: Aways 0 (Sucess)
- */
-int main(void)
-{
-	char ch = 'a';
-
-	while (ch <= 'z')
-		if (ch != 'e' && ch != 'q')
-	{
-		putchar (ch);
-	{
-		ch++;
-	}
-		putchar('\n');
-	}
-		return (0);
+#include <stdio.h>                                                                                                                     
+/**                                                                                                                                    
+* main -Entry point                                                                                                                    
+*                                                                                                                                      
+* Return: Always 0 (Success/correct)                                                                                                   
+                                                                                                                                       
+int main(void)                                                                                                                         
+                                                                                                                                       
+{                                                                                                                                      
+char alphabet;                                                                                                                         
+                                                                                                                                       
+for (alphabet = 'a' <= 'z'; alphabet++)                                                                                                
+{                                                                                                                                      
+if (alphabet == 'q')                                                                                                                   
+continue;                                                                                                                              
+                                                                                                                                       
+else if (alphabet == 'e')                                                                                                              
+continue;                                                                                                                              
+                                                                                                                                       
+putchar(alphabet);                                                                                                                     
+}                                                                                                                                      
+                                                                                                                                       
+putchar('\n');                                                                                                                         
+                                                                                                                                       
+return(0);                                                                                                                             
+                                                                                                                                       
 }
