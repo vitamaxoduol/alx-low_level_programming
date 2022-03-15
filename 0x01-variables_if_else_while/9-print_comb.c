44c6ad37bc397f256169bc389a951107e8a6ddb7
vitamax <vitamaxoduo@gmail.com>
2022-03-15 02:13:35 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,9 +12,9 @@ int main(void)
 	{
 		putchar(n);
 		if (n != 57)
-		{														putchar(',');
-																	putchar(' ');
-		}												
+		{
+			putchar(',');												    putchar(' ');
+		}											
 	}
 	putchar('\n');
 	return (0);
