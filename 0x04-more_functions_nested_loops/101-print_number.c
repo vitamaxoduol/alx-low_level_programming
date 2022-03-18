3b74759cd28d9549c99ffca6cfb3d69a8e705004
vitamax <vitamaxoduo@gmail.com>
2022-03-17 18:22:18 -0700

    numbers have got life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,3 +6,15 @@
 */
 void print_number(int n)
 {
+print_number(98);
+_putchar('\n');
+print_number(402);
+_putchar('\n');
+print_number(1024);
+_putchar('\n');
+ print_number(0);
+_putchar('\n');
+print_number(-98);
+_putchar('\n');
+return (0);
+}
