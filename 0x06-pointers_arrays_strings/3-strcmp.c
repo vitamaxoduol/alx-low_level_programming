56d8b48f4295ed59202a61441fb6f1d15acfb0df
vitamaxoduol <vitamaxoduo@gmail.com>
2022-03-25 02:33:52 +0300

    string comparison

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strcmp - compares two strings
+ *@s1: A pointer to a character will be changed
+ *@s2: A pointer to a character will also be changed 
+ *Return: dest
+ */
+ 
+ int _strcmp(char *s1, char *s2);
+ 
+ {
+
+ char *s1_one = s1;
+ char *s2_two = s2;
+
+
+ while (*str_one != '0\' && *str_two != '0\' && *str_one == *str_two)
+ {
+ str_one++;
+ str_two++;
+ }
+
+ return (*str_one - *str_two);
+ }
