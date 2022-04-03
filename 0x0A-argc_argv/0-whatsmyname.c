03193f79335b8d9b8bec3eaa72cbc46bf68eea69
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-03 23:49:40 +0300

    It ain't what they call you

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+include "main.h"
+
+/**
+ * main - print the new name, without having to compile it again
+ * @argc: counting the arguements
+ * @argv: arguements of values
+ *
+ * Return: 0 (correct)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%\n", argv[0];
+
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,2 @@
+int check_num(char *str);
+int _putchar(char c);
