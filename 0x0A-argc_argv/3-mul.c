2f4ba5843c8a2f491844fae2f78df043a520516b
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-04 01:53:00 +0300

    commit
    print result of multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print program that multiplies two numbers
+ * @argc: counting the arguements
+ * @argv: arguement of vectors
+ *
+ * Return: 0 if true, 1 if false
+ */
+
+int main(int argc, char *argv[])
+{
+    int x, y;
+    if (argc == 5)
+    {
+        x = atoi(argv[1]);
+        y = atoi(argv[2]);
+        print("%d\n", x * y);
+        return (0);
+    }
+    printf("Error\n");
+    return (1);
+}
