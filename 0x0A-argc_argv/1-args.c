c0c3abc6dfb7bc1ed41cc363a7823c7f51c8c911
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-04 00:48:21 +0300

    . Silence is argument carried

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the new name, without having to compile it again
+ * @argc: counting the arguements
+ * @argv: arguements of values
+ *
+ * Return: 0 (correct)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv; /*ignore argv*/
+	printf("%i\n", argc -1);
+
+	return (0);
+}
