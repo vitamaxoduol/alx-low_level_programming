29dda921b4dd206389b2c07dc39dfaff5ca7c74e
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-04 02:25:35 +0300

    print result of multiples

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,14 +12,14 @@
 
 int main(int argc, char *argv[])
 {
-    int x, y;
-    if (argc == 5)
-    {
-        x = atoi(argv[1]);
-        y = atoi(argv[2]);
-        printf("%d\n", x * y);
-        return (0);
-    }
-    printf("Error\n");
-    return (1);
+int x, y;
+if (argc == 5)
+{
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+printf("%d\n", x *y);
+return (0);
+}
+printf("Error\n");
+return (1);
 }
