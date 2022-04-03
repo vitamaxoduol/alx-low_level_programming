1323b2c1b8cd48f988e3a16ef5bdb1a2e43af644
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-04 01:11:49 +0300

    compilation terminated

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the result of the multiplication
+ * @argc: counting the arguements
+ * @argv: arguements of values
+ *
+ * Return: 0 (correct)
+ */
+
+int main(int argc, char *argv[])
+{
+	/*Declares variables*/
+	int i = 0;
+
+	if (argc > 0)
+	{
+	/*To print each arguement*/
+	while (i < argc)
+		{
+		printf("%s\n", argv[i]);
+		i++;
+		}
+	}
+	return (0)
+}
