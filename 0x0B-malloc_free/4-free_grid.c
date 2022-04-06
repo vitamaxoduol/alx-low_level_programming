993b2d9a9b19b83dbc4fd97d606dc5cce2164dbb
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-07 02:06:16 +0300

    Create 4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+ulien@ubuntu:~/0x0a. malloc, free$ cat 4-main.c
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid- frees a 2 dimensional grid
+ * @grid: the multidimensional array of integers
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+void print_grid(int **grid, int width, int height)
+{
+    if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
+}
