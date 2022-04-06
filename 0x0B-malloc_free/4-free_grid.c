1e8283fd9dfe79f92fce980ef359e6076ad7592d
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-07 02:20:21 +0300

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -13,8 +13,8 @@ void free_grid(int **grid, int height)
 {
 if (grid != NULL && height != 0)
 {
-		for (; height >= 0; height--)
-			free(grid[height]);
-		free(grid);
+for (; height >= 0; height--)
+free(grid[height]);
+free(grid);
 }
 }
