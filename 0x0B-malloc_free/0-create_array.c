6835d14d153df1ea020b435df9613e08bd82fbaa
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-06 08:01:48 +0300

    commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,14 +17,13 @@ char *create_array(unsigned int size, char c)
     unsigned int iter = 0;
 
     if (size == 0)
-        return (0);
+    return (0);
 
     ptr = malloc(size * sizeof(char));
     if (ptr == NULL)
-        return (0);
+    return (0);
 
     for (; iter < size; iter++)
-        *(ptr + iter) = c;
-        
-    return ptr;
+    *(ptr + iter) = c;    
+    return (ptr);
 }
