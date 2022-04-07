826893dfa49c70b1e6d8b384034ad17694aa120d
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-08 02:07:10 +0300

    more mallocs

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - allocates memory
+ * @b: the array number
+ * Return: 98
+ */
+void *malloc_checked(unsigned int b)
+{
+    void *a;
+    a = malloc(b);
+    if (a == NULL)
+    {
+        exit(98);
+    
+    }
+
+   return (a); 
+}
