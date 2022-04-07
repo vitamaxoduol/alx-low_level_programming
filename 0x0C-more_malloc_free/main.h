cca4a88460d19f12e567baf6941c017ddd57cc6c
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-08 02:20:53 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,4 @@
-ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 #include "main.h"
 #include <stdio.h>
@@ -10,4 +10,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-endif
+#endif
