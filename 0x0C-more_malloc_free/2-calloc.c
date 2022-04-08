34f4b935ca55b464cc3e88d423caf5a82a7cc8f7
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-08 03:18:03 +0300

    Create 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: the number of values in array
+ * @size: byte for each position in array
+ * Return: void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *c;
+unsigned int i;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+
+c = malloc(nmemb * size);
+if (c == NULL)
+return (NULL);
+
+for (i = 0; i < nmemb * size; i++)
+c[i] = 0;
+
+return (c);
+
+
+}
