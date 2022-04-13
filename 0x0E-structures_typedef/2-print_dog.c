bb2c03a1f8b0efb2391b82c625d0c4f761d41b92
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-13 11:26:08 +0300

    commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,25 +4,25 @@
 /**
  * print_dog - prints a struct dog
  * @d: the dog to print
- * 
+ *
  * Return: void
  */
 
- void print_dog(struct dog *d)
+void print_dog(struct dog *d)
 {
-    if (d == 0)
-    return;
-    
-    if (d->name == 0)
-    printf("Name: (nil)\n");
+if (d == 0)
+return;
+   
+if (d->name == 0)
+printf("Name: (nil)\n");
 
-    else
-    printf("Name: %s\n", d->name);
-    printf("Age: %f\n", d->age);
+else
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
 
-    if (d->owner == 0)
-    printf("owner: (nil)\n");
+if (d->owner == 0)
+printf("owner: (nil)\n");
 
-    else
-    printf("owner: %s\n",d->owner);
+ else
+printf("owner: %s\n", d->owner);
 }
