958406e6a6091e710156b45d3b9ed8314e93d569
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-13 13:03:48 +0300

    commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,53 +1,83 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
- * new_dog - creates a new dog.
- * @name: name of the dog.
- * @age: age of the dog.
- * @owner: owner of the dog.
- *
- * Return: struct dog.
- * if fails, returns NULL.
+ * _copy  -   Make a copy of passed in argument
+ * @src:      Data to make copy of
+ * Return:    Pointer
  */
+
+char *_copy(char *src)
+{
+char *ptr;
+int i, len;
+
+if (src == NULL)
+{
+return (NULL);
+}
+
+for (len = 0; src[len] != '\0'; len++)
+;
+
+ptr = malloc(sizeof(char) * (len + 1));
+
+if (ptr == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; src[i] != '\0'; i++)
+{
+ptr[i] = src[i];
+}
+
+ptr[i] = '\0';
+return (ptr);
+}
+
+/**
+ * new_dog     - Create a new dog variable
+ * @name:        Name of the dog
+ * @age:         Age of the dog
+ * @owner:       Owner of the dog
+ * Return:       Pointer to new dog variable
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *p_dog;
-    int i, lname, lowner;
+	dog_t *cowell;
+	char *new_name, *new_owner;
 
-    p_dog = malloc(sizeof(*p_dog));
-    if (p_dog == NULL || !(name) || !(owner))
-    {
-        free(p_dog);
-        return (NULL);
-    }
+	if (name == NULL || owner == NULL)
+{
+return (NULL);
+}
 
-    for (lname = 0; name[lname]; lname++)
-        ;
+cowell = malloc(sizeof(dog_t));
+if (cowell == NULL)
+{
+return (NULL);
+}
 
-    for (lowner = 0; owner[lowner]; lowner++)
-        ;
+new_name = _copy(name);
+if (new_name == NULL)
+{
+free(cowell);
+return (NULL);
+}
+(*cowell).name = new_name;
 
-    p_dog->name = malloc(lname + 1);
-    p_dog->owner = malloc(lowner + 1);
+(*cowell).age = age;
 
-    if (!(p_dog->name) || !(p_dog->owner))
-    {
-        free(p_dog->owner);
-        free(p_dog->name);
-        free(p_dog);
-        return (NULL);
-    }
+new_owner = _copy(owner);
+if (new_owner == NULL)
+{
+free((*cowell).name);
+free(cowell);
+return (NULL);
+}
+(*cowell).owner = new_owner;
 
-    for (i = 0; i < lname; i++)
-        p_dog->name[i] = name[i];
-    p_dog->name[i] = '\0';
-
-    p_dog->age = age;
-
-    for (i = 0; i < lowner; i++)
-        p_dog->owner[i] = owner[i];
-    p_dog->owner[i] = '\0';
-
-    return (p_dog);
+return (cowell);
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
  * free_dog - free memory
- *@d: array
+ * @d: array
  * Description: Free memory for struct
  */
 
 void free_dog(dog_t *d)
 {
-    if (d)
-    {
-        free(d->name);
-        free(d->owner);
-        free(d);
-    }
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
 }
