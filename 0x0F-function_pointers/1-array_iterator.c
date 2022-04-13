7e3e6edd7e1cff69c34418af6fdd0823e5c4a2b1
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-14 02:05:14 +0300

    commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,7 +16,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 unsigned int i;
 
 if (array && action)
-for (i = 0; i < size;)
+for (i = 0; i < size)
 i++;
+
 action(array[i]);
 }
