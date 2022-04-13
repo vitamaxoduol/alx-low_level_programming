5ecaeab6e278041c4f408a2ee11bc5bc321c6901
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-13 11:27:01 +0300

    commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 {
 if (d == 0)
 return;
-   
+ 
 if (d->name == 0)
 printf("Name: (nil)\n");
 
@@ -23,6 +23,6 @@ printf("Age: %f\n", d->age);
 if (d->owner == 0)
 printf("owner: (nil)\n");
 
- else
+else
 printf("owner: %s\n", d->owner);
 }
