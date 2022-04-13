652ba16cb8cbb669a52f26c4159a64c7efac1a5c
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-13 11:42:20 +0300

    commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -22,9 +22,9 @@ printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
 
 if (d->owner == 0)
-printf("owner: (nil)\n");
+printf("Owner: (nil)\n");
 
 else
-printf("owner: %s\n", d->owner);
+printf("Owner: %s\n", d->owner);
 
 }
