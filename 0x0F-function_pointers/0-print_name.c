1cd40fcddf8a735df1079d2a3b80af3478ace9f8
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-14 01:42:19 +0300

    commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,11 +2,11 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: name of the person
- * @f: function of a pointer
+ * print_name - prints a name.
+ * @name: name of the person.
+ * @f: function of a pointer.
  * 
- * Return: Nothing 
+ * Return: Nothing. 
  */
 
 void print_name(char *name, void (*f)(char *))
