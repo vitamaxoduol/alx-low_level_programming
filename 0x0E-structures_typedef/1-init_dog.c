3c055b8cd3bd345e7ca6d76e145f051277eceaf7
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-13 10:42:19 +0300

    commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,29 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * init_dog - Struct dog
+ * @d: dog
+ *@name: name
+ * @age: age
+ * @owner: owner
+ * Description: This struct is for dog 
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+struct dog *d;
+char *name;
+float age;
+char *owner;
+if (d == NULL)
+return;
+
+else
+{
+d->name;
+d->age;
+d->owner;
+}
+
+}
