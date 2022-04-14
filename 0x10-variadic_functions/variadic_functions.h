e5284a098ade2f09919d53083d94ff20d5f8f9b9
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-15 02:41:36 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
@@ -21,6 +22,7 @@ typedef struct printTypeStruct
 {
 char *type;
 void (*printer)(va_list);
-} printTypeStruct;
+} 
+printTypeStruct;
 
 #endif
