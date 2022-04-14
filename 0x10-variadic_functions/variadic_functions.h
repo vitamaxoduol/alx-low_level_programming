e85669b74e84750b2a7df68887611c73c92d2d50
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-14 21:10:59 +0300

    commit variadic_functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
