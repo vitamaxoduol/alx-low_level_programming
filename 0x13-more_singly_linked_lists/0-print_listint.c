0d9eb4056df83863fdd30250a4357f1d5f4293a6
vitamaxoduol <vitamaxoduo@gmail.com>
2022-04-27 04:57:27 +0300

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a list
+ *
+ * @h: the list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t num_of_nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n",h->n);
+		h = h->next;
+		num_of_nodes++;
+	}
+	return (num_of_nodes);
+}
